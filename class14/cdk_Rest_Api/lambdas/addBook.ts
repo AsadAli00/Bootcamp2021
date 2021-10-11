0cc386c3a2d84cab7fdc576094592b56ee05acd3
Asad Ali <asadali111222@outlook.com>
2021-10-11 13:39:02 +0500

    update addbook.ts in Class14

--- class14/cdk_Rest_Api/lambdas/addBook.ts
+++ class14/cdk_Rest_Api/lambdas/addBook.ts
@@ -1,5 +1,5 @@
-import * as AWS from 'aws-sdk';
-import {v4 as uuidv4 } from 'uuid';
+import * as AWS from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
 
 const TABLE_NAME = process.env.TABLE_NAME || "";
 const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
@@ -7,27 +7,27 @@ const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
 const db = new AWS.DynamoDB.DocumentClient();
 
 export const handler = async (event: any = {}): Promise<any> => {
-    if (!event.body) {
-      return {
-        statusCode: 400,
-        body: "invalid request, you are missing the parameter body",
-      };
-    }
-    const item =
-      typeof event.body == "object" ? event.body : JSON.parse(event.body);
-    item[PRIMARY_KEY] = uuidv4();
-    const params = {
-      TableName: TABLE_NAME,
-      Item: item,
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: "invalid request, you are missing the parameter body",
     };
-  
-    try {
-      await db.put(params).promise();
-      return { statusCode: 201, body: "" };
-    } catch (err) {
-      console.log("DynamoDB error: ", err);
-      return { statusCode: 500, body: err };
-    }
+  }
+  const item =
+    typeof event.body == "object" ? event.body : JSON.parse(event.body);
+  item[PRIMARY_KEY] = uuidv4();
+  const params = {
+    TableName: TABLE_NAME,
+    Item: item,
   };
+
+  try {
+    await db.put(params).promise();
+    return { statusCode: 201, body: "" };
+  } catch (err) {
+    console.log("DynamoDB error: ", err);
+    return { statusCode: 500, body: err };
+  }
+};
 
 
